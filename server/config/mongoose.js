580ec7e0e23d0afc1a9f3855be84576f3af017b9
steven <slstevenlam@gmail.com>
2015-11-30 13:28:25 -0800

    npm install

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -2,7 +2,14 @@ var mongoose = require('mongoose');
 // require file-system so that we can load, read, require all of the model files
 var fs = require('fs');
 // connect to the database
-mongoose.connect('mongodb://localhost/discussion_board');
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+if (process.env.NODE_ENV === 'development') {
+  mongoose.connect('mongodb://localhost/discussion_board');`
+} else {
+  mongoose.connect('mongodb://<some_production_mongo_url>');`
+}
+
 // specify the path to all of the models
 var models_path = __dirname + '/../models'
 // read all of the files in the models_path and for each one check if it is a javascript file before requiring it
